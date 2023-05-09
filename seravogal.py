a8d32fca3d0e4755f3d647d4f93830a70fc201fb
anaju68 <129894377+anaju68@users.noreply.github.com>
2023-05-09 15:10:36 -0300

    Add files via upload

---
+++ seravogal.py
@@ -0,0 +1,11 @@
+letrinha=input("Digite uma letra:\n")
+if letrinha=="a":
+    print("Você digitou uma vogal")
+elif letrinha=="e":
+    print("Você digitou uma vogal")
+elif letrinha=="i":
+    print("Você digitou uma vogal")
+elif letrinha=="o":
+    print("Você digitou uma vogal")
+else:
+    print("Você digitou uma consoante")
